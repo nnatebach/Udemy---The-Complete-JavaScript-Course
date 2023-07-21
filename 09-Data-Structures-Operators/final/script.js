611363cf08c933730c6d4de1d7b27bbb3d55c18f
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-07-21 15:54:27 +0700

    Enhanced Object Literals

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -4,6 +4,24 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri']
+const weekends = ['sat', 'sun']
+
+const openingHours = {
+  [weekdays[4]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[3]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekends[0]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+}
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,30 +30,19 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (startIndex, mainIndex) {
+  // ES6 enhanced object literals
+  openingHours,
+  
+  order (startIndex, mainIndex) {
     return [this.starterMenu[startIndex], this.mainMenu[mainIndex]]
   },
-  orderDelivery: function ({startIndex=1, mainIndex=0, time="20:00", address}) {
+  orderDelivery ({startIndex=1, mainIndex=0, time="20:00", address}) {
     console.log(`Order received! ${this.starterMenu[startIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta (ing1, ing2, ing3) {
     console.log(`The ingredients to make pasta includes ${ing1}, ${ing2}, ${ing3}`);
   },
-  orderPizza: function(mainIngredient, ...otherIngredients) {
+  orderPizza (mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   }
