1ca15d10376de68be990c645fa6444e8c879d2d8
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-07-31 11:34:00 +0700

    [add] Functions Accepting Callback Functions

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -64,32 +64,91 @@
 ////////////////////// How Passing Arguments Works Value vs. Reference - END
 
 
-////////////////////// Functions Returning Functions - START
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  }
+////////////////////// Functions Accepting Callback Functions - START
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 }
-console.log(greet);
-// ƒ (greeting) {
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  // console.log(str.split(' ')); // (4) ['JavaScript', 'is', 'the', 'best!']
+  // console.log([first.toUpperCase(), ...others].join(' ')); // JAVASCRIPT is the best!
+  return [first.toUpperCase(), ...others].join(' ')
+}
+
+////// Higher order function - a function that takes one or more functions as arguments, or returns a function as its result.
+const transformer = function (str, fn) {
+  console.log(`Original string ${str}`);
+  console.log(`Transformed string ${fn(str)}`);
+
+  console.log(`Transformed by ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord)
+
+transformer('JavaScript is the best!', oneWord)
+
+////// console.log orders
+// Original string JavaScript is the best!
+// JAVASCRIPT is the best! // upperFirstWord
+// Transformed string JAVASCRIPT is the best!
+// Transformed by upperFirstWord
+
+// JS uses callback all the time
+const high5 = function () {
+  console.log('✋');
+}
+document.body.addEventListener('click', high5)
+['Jonas', 'Martha', 'Adam'].forEach(high5)
+
+////////////////////// Functions Accepting Callback Functions - END
+
+
+////////////////////// Functions Returning Functions - START
+// const greet = function (greeting) {
 //   return function (name) {
 //     console.log(`${greeting} ${name}`);
 //   }
 // }
-const greeterHey = greet('Hey')
-console.log(greeterHey);
-// ƒ (name) {
-//   console.log(`${greeting} ${name}`);
-// }
-greeterHey('Jonas') // Hey Jonas
-greeterHey('Steven') // Hey Steven
-greet('Hello')('Jonas') // Hello Jonas
+// console.log(greet);
+// // ƒ (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting} ${name}`);
+// //   }
+// // }
+// const greeterHey = greet('Hey')
+// console.log(greeterHey);
+// // ƒ (name) {
+// //   console.log(`${greeting} ${name}`);
+// // }
+// greeterHey('Jonas') // Hey Jonas
+// greeterHey('Steven') // Hey Steven
+// greet('Hello')('Jonas') // Hello Jonas
 
 
-////// Challenge - Change keyword 'function' to using arrow function
+// ////// Challenge - Change keyword 'function' to using arrow function
 
-const greetArr = greeting => name => console.log(`${greeting} ${name}`); // Hi Jonas
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`); // Hi Jonas
 
-greetArr('Hi')('Jonas')
+// greetArr('Hi')('Jonas')
 
 ////////////////////// Functions Returning Functions - END
+
+////////////////////// The call and apply Methods - START
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   // booking () {}
+//   book (flightNum, name) {
+//     console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+//     // Jonas Schmedtmann booked a seat on Lufthansa flight LH623
+//     // Nathan Bach booked a seat on Lufthansa flight LH635
+//   }
+// }
+
+// lufthansa.book(623, 'Jonas Schmedtmann')
+// lufthansa.book(635, 'Nathan Bach')
+////////////////////// The call and apply Methods - END
+
+
