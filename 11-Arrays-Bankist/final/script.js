7b5dd102b77b7bf28b2536d8d315f0bac796a3da
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-08-07 11:30:27 +0700

    [add] 006 forEach With Maps and Sets

--- 11-Arrays-Bankist/final/script.js
+++ 11-Arrays-Bankist/final/script.js
@@ -64,14 +64,16 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+// Move down for 006 forEach With Maps and Sets exercise
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// 'movements' is changed to 'transactions' for the exercise in '005 Looping Arrays forEach'
+// Reason: I found 'transactions' was a better word and also I did not know this array information would be actually used for that exercise
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -122,48 +124,48 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 ////////////////////////// 004 The new 'at' method - START
 //////////// it does not change the original data
-const arr = [23,11,64]
-const string = ['abc', 'cde', 'def', 'efg', 'fgi', 'ghi']
-console.log(arr[0]); // 23
-console.log(arr.at[0]); // undefined
+// const arr = [23,11,64]
+// const string = ['abc', 'cde', 'def', 'efg', 'fgi', 'ghi']
+// console.log(arr[0]); // 23
+// console.log(arr.at[0]); // undefined
 
 // getting last array element
-console.log(arr[arr.length-1]); // 64
-console.log(arr.slice(-1)); // [64] => array with only 1 element
-console.log(arr.slice(-1)[0]); // 64
-console.log(arr.at(-1)); // 64
-console.log(arr.at(-2)); // 11
-console.log(arr); // (3) [23, 11, 64]
+// console.log(arr[arr.length-1]); // 64
+// console.log(arr.slice(-1)); // [64] => array with only 1 element
+// console.log(arr.slice(-1)[0]); // 64
+// console.log(arr.at(-1)); // 64
+// console.log(arr.at(-2)); // 11
+// console.log(arr); // (3) [23, 11, 64]
 
-console.log(string.slice(-2)); // n
+// console.log(string.slice(-2)); // n
 
-console.log(typeof 'bach'); // string
-console.log('bach'.slice(-1)); // h
-console.log('bach'.slice(-3)); // ach
-console.log('bach'.slice(-1)[0]); // h
-console.log('bach'.at(-1)); // h
-console.log('bach'.at(0)); // b
+// console.log(typeof 'bach'); // string
+// console.log('bach'.slice(-1)); // h
+// console.log('bach'.slice(-3)); // ach
+// console.log('bach'.slice(-1)[0]); // h
+// console.log('bach'.at(-1)); // h
+// console.log('bach'.at(0)); // b
 ////////////////////////// 004 The new 'at' method - END
 
 
 ////////////////////////// 005 Looping Arrays forEach - START
 
-const transactions = [200,450,-400,3000,-650,-130,70,1300]
+// const transactions = [200,450,-400,3000,-650,-130,70,1300]
 
 // for (const transaction of transactions) {
-for (const [i, transaction] of transactions.entries()) {
-  // console.log(transaction);
-  if (transaction > 0) {
-    console.log(`Transaction ${i+1}: You deposited ${transaction}`);
-  } else {
-    console.log(`Transaction ${i+1}: You withdrew ${Math.abs(transaction)}`);
-  }
-}
+// for (const [i, transaction] of transactions.entries()) {
+//   // console.log(transaction);
+//   if (transaction > 0) {
+//     console.log(`Transaction ${i+1}: You deposited ${transaction}`);
+//   } else {
+//     console.log(`Transaction ${i+1}: You withdrew ${Math.abs(transaction)}`);
+//   }
+// }
 
 // The absolute value of a number 'x'. If x is negative (including -0), returns -x. Otherwise, returns x. The result is therefore always a positive number or 0.
 
 ////////////// forEach
-console.log('----------------- forEach -----------------');
+// console.log('----------------- forEach -----------------');
 // transactions.forEach( function (transaction) {
 //   if (transaction > 0) {
 //     console.log(`You deposited ${transaction}`);
@@ -176,13 +178,13 @@ console.log('----------------- forEach -----------------');
 // First value: current element
 // Second value: index
 // Third value: the entire array that we are looping
-transactions.forEach(function(transaction, i, arr){
-  if (transaction > 0) {
-    console.log(`Transaction ${i+1}: You deposited ${transaction}`);
-  } else {
-    console.log(`Transaction ${i+1}: You withdrew ${Math.abs(transaction)}`);
-  }
-})
+// transactions.forEach(function(transaction, i, arr){
+//   if (transaction > 0) {
+//     console.log(`Transaction ${i+1}: You deposited ${transaction}`);
+//   } else {
+//     console.log(`Transaction ${i+1}: You withdrew ${Math.abs(transaction)}`);
+//   }
+// })
 
 // 0: function(200)
 // 1: function(450)
@@ -195,7 +197,46 @@ transactions.forEach(function(transaction, i, arr){
 // While the names of the parameters are not important YET the orders of the parameters ARE important.
 
 // What is the need for the parameter 'arr'???
-// Reason: This a sample showing that we can use all 3 parameters here
+// Reason: If your callback function were declared elsewhere then it has no idea what array it's being used for
+// Read more at 'https://stackoverflow.com/questions/43500396/what-is-point-of-third-parameter-in-foreach-callback-function-in-javascript'
 
 ////////////////////////// 005 Looping Arrays forEach - END
+
+
+////////////////////////// 006 forEach With Maps and Sets - START
+///////////// MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+// USD: United States dollar
+// The first argument is the 'current value' in the 'current iteration'
+// The second argument is the 'key'
+// The third argument is the entire map that is looped over
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+  // USD: United States dollar
+  // EUR: Euro
+  // GBP: Pound sterling
+})
+// Question: Why there is the difference between the orders of 'key' and 'value' between the parameters and when logging them out from the console?
+
+///////////// SET
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+// console.log(currenciesUnique); // Set(3) {'USD', 'GBP', 'EUR'}
+
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+  // USD: USD
+  // GBP: GBP
+  // EUR: EUR
+
+  // Reason: SET does NOT have neither the 'key' nor the 'indices' => NO value would make sense for the 'key'
+  // Solution: Replace the 'key' with the underscore '_' which is a throw away variable, a convention in JavaScript
+  // Read more: https://stackoverflow.com/questions/11406823/underscore-as-a-javascript-variable
+})
+
+////////////////////////// 006 forEach With Maps and Sets - END
 
