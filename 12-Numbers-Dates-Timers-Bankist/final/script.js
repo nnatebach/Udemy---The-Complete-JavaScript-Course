f2d778b0e255884e164ec806eeb8239515408554
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-08-21 17:47:29 +0700

    [add] 004 Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/final/script.js
+++ 12-Numbers-Dates-Timers-Bankist/final/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -111,12 +111,12 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,55 +252,115 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0); // true
+////////////////////////////////// 003 Converting and Checking Numbers - START
 
-// Base 10 - 0 to 9. 1 / 10 = 0.1. 3 / 10 = 3.33333333
-// Binary base 2 - 0 1
-console.log(.1 + .2); // 0.30000000000000004
-console.log(.1 + .2 === .3); // false
+// console.log(23 === 23.0); // true
 
-// Conversion
-console.log(Number(23)); // 23
-console.log(+'23'); // 23
+// // Base 10 - 0 to 9. 1 / 10 = 0.1. 3 / 10 = 3.33333333
+// // Binary base 2 - 0 1
+// console.log(.1 + .2); // 0.30000000000000004
+// console.log(.1 + .2 === .3); // false
 
-// Parsing
-console.log(Number.parseInt('30px')); // 30
-console.log(Number.parseInt('e23')); // NaN
+// // Conversion
+// console.log(Number(23)); // 23
+// console.log(+'23'); // 23
 
-console.log(Number.parseInt('2.5rem')); // 2
-// This is the GO TO method whenever you need to read a value out of a string
-console.log(Number.parseFloat('2.5rem')); // 2.5
+// // Parsing
+// console.log(Number.parseInt('30px')); // 30
+// console.log(Number.parseInt('e23')); // NaN
 
-// This function here is also a global function => We would not have to call it on 'Number', that would also work.
-// However, this is the traditional way. In modern JavaScript, it is more encouraged to call the function 'parseFloat' on the 'Number' object.
-// console.log(parseFloat('2.5rem')); // 2.5
+// console.log(Number.parseInt('2.5rem')); // 2
+// // This is the GO TO method whenever you need to read a value out of a string
+// console.log(Number.parseFloat('2.5rem')); // 2.5
 
-////// Check if value is NaN
-//// The isNaN() function determines whether a value is NaN
-// - first converting the value to a number if necessary.
-// - Because coercion inside the isNaN() function can be surprising, you may prefer to use Number.isNaN().
-console.log(Number.isNaN(20)); // false
-console.log(Number.isNaN('20')); // false
+// // This function here is also a global function => We would not have to call it on 'Number', that would also work.
+// // However, this is the traditional way. In modern JavaScript, it is more encouraged to call the function 'parseFloat' on the 'Number' object.
+// // console.log(parseFloat('2.5rem')); // 2.5
 
-// Converting the '20X' string to a number (by adding the 'plus' sign (+) before it) => Not a number
-console.log(Number.isNaN(+'20X')); // true
+// ////// Check if value is NaN
+// //// The isNaN() function determines whether a value is NaN
+// // - first converting the value to a number if necessary.
+// // - Because coercion inside the isNaN() function can be surprising, you may prefer to use Number.isNaN().
+// console.log(Number.isNaN(20)); // false
+// console.log(Number.isNaN('20')); // false
 
-// Diving a number by zero (0) is NOT allowed in mathematics since that will give infinity
-console.log(Number.isNaN(23 / 0)); // false
+// // Converting the '20X' string to a number (by adding the 'plus' sign (+) before it) => Not a number
+// console.log(Number.isNaN(+'20X')); // true
 
-
-////// Check if value is finite
-// isFinite - A better method for testing finite value
-// The isFinite() function determines whether a value is finite, first converting the value to a number if necessary. A finite number is one that's not NaN or ±Infinity. Because coercion inside the isFinite() function can be surprising, you may prefer to use Number.isFinite().
-// This is the GO TO method to check whether something is a number or not.
-console.log(Number.isFinite(20)); // true
-console.log(Number.isFinite('20')); // false
-console.log(Number.isFinite(+'20X')); // false
-console.log(Number.isFinite(23 / 0)); // false
+// // Diving a number by zero (0) is NOT allowed in mathematics since that will give infinity
+// console.log(Number.isNaN(23 / 0)); // false
 
 
-////// Check if value is an Integer
-// The Number.isInteger() static method determines whether the passed value is an integer.
-console.log(Number.isInteger(23)); // true
-console.log(Number.isInteger(23.0)); // true
-console.log(Number.isInteger(23 / 0)); // false
+// ////// Check if value is finite
+// // isFinite - A better method for testing finite value
+// // The isFinite() function determines whether a value is finite, first converting the value to a number if necessary. A finite number is one that's not NaN or ±Infinity. Because coercion inside the isFinite() function can be surprising, you may prefer to use Number.isFinite().
+// // This is the GO TO method to check whether something is a number or not.
+// console.log(Number.isFinite(20)); // true
+// console.log(Number.isFinite('20')); // false
+// console.log(Number.isFinite(+'20X')); // false
+// console.log(Number.isFinite(23 / 0)); // false
+
+
+// ////// Check if value is an Integer
+// // The Number.isInteger() static method determines whether the passed value is an integer.
+// console.log(Number.isInteger(23)); // true
+// console.log(Number.isInteger(23.0)); // true
+// console.log(Number.isInteger(23 / 0)); // false
+
+////////////////////////////////// 003 Converting and Checking Numbers - END
+
+
+////////////////////////////////// 004 Math and Rounding - START
+
+//// Math.sqrt() static method returns the square root of a number.
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1/2)); // 5
+console.log(8 ** (1/3)); // 2
+
+//// Math.max() static method returns
+// - the largest of the numbers given as input parameters,
+// - or -Infinity if there are no parameters.
+console.log(Math.max(5,18,23,11,2)); // 23
+console.log(Math.max(5,18,'23',11,2)); // 23
+console.log(Math.max(5,18,'23px',11,2)); // NaN
+
+console.log(Math.min(5,18,23,11,2)); // 2
+
+console.log(Math.PI); // 3.141592653589793
+
+// Calculate the area of a circle with the radius of 10px
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+//// Generate 6 different random numbers - The Dice example
+// - Math.trunc - returns the integer part of a number by removing any fractional digits.
+// - Plus 1 ( + 1) at the end because we want to have 6 numbers from 1 - 6
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+//// Generate random numbers given the range of min and max
+const randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + 1) + min
+// 0...1 -> 0...(max - min) -> min...max
+console.log(randomInt(10, 20)); // min value being 10 + 1 and max value is 20
+
+// Rounding integers
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor('23.9')); // 23
+
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding decimals - The toFixed() method of Number values formats this number using fixed-point notation.
+console.log((2.7).toFixed(0)); // 3
+console.log((2.7).toFixed(3)); // 2.700
+console.log((2.345).toFixed(2)); // 2.35
+console.log(+(2.345).toFixed(2)); // 2.35 => number - the plus '+' sign converts a string to a number
+
+////////////////////////////////// 004 Math and Rounding - END
+
