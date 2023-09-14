72e99e609254abaff0428188da69cc7ab3e378fc
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-09-14 13:44:24 +0700

    [add] 003 Asynchronous JavaScript, AJAX and APIs

--- 16-Asynchronous/final/script.js
+++ 16-Asynchronous/final/script.js
@@ -4,3 +4,41 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+
+/////////////////////////////////////// 003 Asynchronous JavaScript, AJAX and APIs - START
+
+
+//////////// Most code is SYNCHRONOUS
+// Synchronous code is EXECUTED LINE BY LINE
+// Each line of code WAITS for previous line to finish
+// DOWNSIDE: Long-running operations BLOCK code execution
+
+//////////// ASYNCHRONOUS
+// Asynchronous code is executed AFTER A TASK THAT RUNS IN THE 'BACKGROUND' FINISHES
+// Asynchronous code is NON-BLOCKING
+// Execution does not wait for an asynchronous task to finish its work
+// Callback functions alone do NOT make code asynchronous!
+
+//////////// AJAX - Asynchronous JavaScript and XML
+// allows us to communicate with remote web servers in an ASYNCHRONOUS WAY.
+// we can REQUEST DATA from web servers dynamically
+// XML - the data format that used to be widely used to transmit data on the web
+// Nowadays, AJAX has replaced XML with JSON data format for data transmission (JSON is a JavaScript object-based String)
+
+//////////// API - Application Programming Interface (software)
+// can be used by another piece of software, in order to allow APPLICATIONS TO TALK TO EACH OTHER
+// different types of APIs in web development: DOM API, Geolocation API, Own Class API, "Online" API
+// "Online" API - Application running on a server, that receives requests for data, and sends data back as response.
+// we can build OUR OWN web APIs (requires back-end development, e.g. with node.js) or use 3RD-PARTY APIs
+// - Weather data
+// - Data about countries
+// - Flights data
+// - Currency conversion data
+// - APIs for sending email or SMS
+// - Google Maps
+// - Millions of possibilities
+
+
+/////////////////////////////////////// 003 Asynchronous JavaScript, AJAX and APIs - END
+
