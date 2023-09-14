03bae6998eee64b55db5dfdca1045f3b156868e3
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-09-14 22:18:18 +0700

    [add] 007 Welcome to Callback Hell - nested AJAX call

--- 16-Asynchronous/final/script.js
+++ 16-Asynchronous/final/script.js
@@ -45,7 +45,7 @@ const countriesContainer = document.querySelector('.countries');
 
 /////////////////////////////////////// 005 Our First AJAX Call XMLHttpRequest - START
 
-
+/*
 //////////// XMLHttpRequest (XHR) objects - used to interact with servers.
 
 const getCountry = function (country) {
@@ -80,7 +80,79 @@ const getCountry = function (country) {
 getCountry('portugal')
 getCountry('vietnam')
 getCountry('canada')
-
+*/
 
 /////////////////////////////////////// 005 Our First AJAX Call XMLHttpRequest - END
+
+
+/////////////////////////////////////// 006 [OPTIONAL] How the Web Works Requests and Responses - START
+
+/*********** NOTE LATER ***********/
+
+/////////////////////////////////////// 006 [OPTIONAL] How the Web Works Requests and Responses - END
+
+
+/////////////////////////////////////// 007 Welcome to Callback Hell - START
+
+// const renderCountry = function (data) {
+const renderCountry = function (data, className="") { // attach className to a neighbor country
+  const html = `
+  <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html)
+  countriesContainer.computedStyleMap.opacity = 1
+}
+
+const getCountryAndNeighbor = function (country) {
+
+  //////////////// AJAX call country 1
+  const request = new XMLHttpRequest()
+
+  request.open('GET', `https://restcountries.com/v2/name/${country}`)
+  request.send()
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText)
+    console.log(data);
+
+    //////////////// Render country 1
+    renderCountry(data)
+
+    //////////////// Get neighbor country (2)
+    const neighborCountry = data.borders?.[0]
+    if (!neighborCountry) return
+    //////////////// AJAX call country 2
+    const request2 = new XMLHttpRequest()
+
+    // we want to get the 'borders' yet it comes with the code ("KHM", "CHN", "LAO") and not the country name => we need to use the code to access it
+    // the second AJAX call would not have been possible without the first AJAX call
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighborCountry}`)
+    request2.send()
+
+    // the second 'addEventListener' inside the first one => the second callback inside the first callback => nested callback
+    request2.addEventListener('load', function() {
+      // console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText)
+      // canNOT destructure here => The 'responseText' here is NO longer an array when we search for the code ('alpha') => this time we are search for country code, NOT country name.
+      // the country codes are unique => There is ALWAYS just ONE result! => NO array is returned!
+
+      console.log(data2);
+
+      // renderCountry(data2)
+      renderCountry(data2, 'neighbour') // attach className to a neighbor country
+    })
+  });
+}
+getCountryAndNeighbor('vietnam')
+
+/////////////////////////////////////// 007 Welcome to Callback Hell - END
 
