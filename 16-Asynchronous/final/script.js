373f9b06773ae9da0f8c8c976e0f79e471e5e037
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-10 00:53:18 +0700

    [add] 024 Coding Challenge #3

--- 16-Asynchronous/final/script.js
+++ 16-Asynchronous/final/script.js
@@ -854,6 +854,7 @@ get3Countries('Vietnam', 'Laos', 'Thai')
 /////////////////////////////////////// 023 Other Promise Combinators race, allSettled and any - START
 
 
+/*
 // NOTE: Keep in mind AT LEAST 'Promise.race()' and 'Promise.all()'
 
 (async function () {
@@ -952,6 +953,162 @@ Promise.all([
 // If one of the Promise is rejected, the whole thing is going to be 'rejected' => ERROR
 
 ////// 'Promise.allSettled()' AND 'Promise.all() - END
+*/
 
 
 /////////////////////////////////////// 023 Other Promise Combinators race, allSettled and any - END
+
+
+/////////////////////////////////////// Coding Challenge #3 - START
+
+
+/*
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK
+*/
+
+
+/*
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+*/
+
+
+const wait = function (seconds) {
+  return new Promise (function (resolve) {
+    setTimeout(resolve, seconds * 1000)
+  })
+}
+
+const imgContainer = document.querySelector('.images')
+
+const createImage = function (imgPath) {
+  return new Promise (function (resolve, reject) {
+    const img = document.createElement('img')
+    img.src = imgPath
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img)
+      resolve(img)
+    })
+    img.addEventListener('error', function() {
+      reject(new Error('Image not found!!'))
+    })
+  })
+}
+
+//// createImage with 'catch'
+
+// let currentImg
+
+/*
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+*/
+
+//// reuse createImage
+
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    // This time we will not need the "currentImg" variable
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    // 'await' for 'wait(2)'
+    // we don't need to store this into any variable
+    await wait(2)
+    img.style.display = 'none';
+
+    // Load image 2
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    // 'await' for 'wait(2)'
+    await wait(2)
+    img.style.display = 'none';
+  } catch(err) {
+    console.error(err);
+  }
+}
+// loadNPause()
+
+
+/*
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'parallel' class to all the images (it has some CSS styles).
+*/
+
+const loadAll = async function (imgArr) {
+  try {
+    // in each iteration of the array we have one image
+    // we want to use the 'createImage' function to load that image
+    // the function 'createImage' returns a Promise => we need to 'await' that Promise
+    // we need to make 'i => createImage(i)' an async function
+    // and 'await' for 'createImage(i)'
+    const imgs = imgArr.map(async i => await createImage(i))
+    console.log(imgs);
+
+    const imgsEL = await Promise.all(imgs)
+    // this works because in this loop 'async i => await createImage(i)', the 'await' pauses the execution of the function => we use 'Promise.all()' to actually get the image elements out of the Promise array
+    // this is how we use 'async await' in a 'map' method
+    // all the images are loaded all at the same time because of this loop 'i => await createImage(i)'
+    console.log(imgsEL);
+    // the three image elements are hidden (check console) => add 'parallel' class
+    imgsEL.forEach(img => img.classList.add('parallel'))
+  } catch (err) {
+    console.error(err);
+  }
+}
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'])
+
+////// NOTE:
+// const imgs = imgArr.map(async i => await createImage(i))
+// (3) [Promise, Promise, Promise]
+// we actually got three 'fulfilled' Promises here AND NOT the images themselves
+// 'async i => await createImage(i)' - here we have an 'async' function, an 'arrow' function and an 'implicit return' => this is like returning something from this callback function '(return) await createImage(i)' in each iteration.
+// An 'async' will always return a Promise WHILE the value that we want to return is going to be the 'fulfilled' value of the Promise that the 'async' function returns.
+// We are using the 'async' function on three images => the Promises are happening three times => we are returning something three times from an 'async' function here => the result will be three Promises
+// In the previous lecture where we attempted to return a string from one of the functions. In here we have the exact same situation!
+// The images are already being loaded behind the scenes => Now we need to get the image elements themselves out of the Promise
+// Option 1: Take each Promise out of the array and then manually await it => NOT a good solution. WHY???
+//// - We would have additional work
+//// - the work would not be happening in parallel, BUT we want it to be happening in parallel
+// Option 2: Promise.all()
+//// - This will take some time
+//// - This can ONLY happen once the images are ACTUALLY LOADED
+
+
+/////////////////////////////////////// Coding Challenge #3 - END
